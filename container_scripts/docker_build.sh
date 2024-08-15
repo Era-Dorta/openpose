2e286991478655153bef8237457500486cb5d1f8
Era Dorta <e.dorta@tudelft.nl>
2024-08-15 10:48:13 +0200

    Add version to docker files

--- container_scripts/docker_build.sh
+++ container_scripts/docker_build.sh
@@ -3,7 +3,9 @@ REPO_DIR=$(dirname $(dirname $(realpath $0)))
 
 cd $REPO_DIR
 
+VERSION=$(grep -oP '(?<=const std::string OPEN_POSE_VERSION_STRING = ")[^"]*' include/openpose/core/macros.hpp)
+
 docker build \
     --progress=plain \
-    --tag eradorta/openpose \
+    --tag eradorta/openpose:$VERSION \
     ${REPO_DIR}
--- container_scripts/docker_run.sh
+++ container_scripts/docker_run.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+REPO_DIR=$(dirname $(dirname $(realpath $0)))
+
+cd $REPO_DIR
+
+VERSION=$(grep -oP '(?<=const std::string OPEN_POSE_VERSION_STRING = ")[^"]*' include/openpose/core/macros.hpp)
+
 # Mount the data folder
 # -v /home/era/code/EasyMocap/data/examples:/home/user/easymocap/EasyMocap/data/examples \
 
@@ -5,4 +12,4 @@ docker run \
     --rm \
     --network=host \
     --gpus all \
-    eradorta/openpose
+    eradorta/openpose:$VERSION
