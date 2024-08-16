a248f2539039ea2c429cf959f194a9aca375e9bd
Era Dorta <e.dorta@tudelft.nl>
2024-08-16 10:25:14 +0200

    Add logger to flask script and report back if error on command execution

--- flask/flask_entrypoint.py
+++ flask/flask_entrypoint.py
@@ -1,25 +1,31 @@
 from flask import Flask, request, jsonify
 import os
-
+import logging
+import subprocess
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/run_command')
 def run_command():
-    print("------- in Run command -------")
-    print(f"------- request{request}")
-    data = request.json
-    received_cmd = data.get('cmd', '')
-    print(f"-------  got cmd {received_cmd}")
+    app.logger.info(f"run_command: route called")
+    
+    received_cmd = request.json.get('cmd', '')
 
-    os.system(received_cmd)
+    app.logger.info(f"run_command: cmd to execute is\n\t{received_cmd}")
 
-    print("------- run command done -------")
-    processed_message = "Processed"
-
-    return jsonify({"output": processed_message})
+    ret = subprocess.run(received_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    
+    app.logger.info(f"run_command: done, return info is\n\t{ret}")
+    
+    if ret.returncode == 0:
+        return jsonify({"output": ret.stdout.decode()})
+    else:
+        return jsonify({"output": ret.stdout.decode()}), 500
 
 @app.route('/shutdown')
 def shutdown():
+    app.logger.info(f"shutdown route called")
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
